6410257a4b30a39101c65c1e26176d969a4866bd
raylaraya <raymondlaraya@gmail.com>
2023-05-31 00:01:10 -0400

    Seeding database with campgrounds

---
+++ seeds/index.js
@@ -0,0 +1,41 @@
+/*
+    We will run this file on its own anytime we want to seed our database
+    which is pretty much anytime we make changes to our model or data
+    Seeding: populating our database with an initial set of data
+    In our case we our populating our mongodb database with initial sets of campgrounds
+*/
+
+const mongoose = require('mongoose');
+const cities = require('./cities')
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+// deletes everything from our database and replaces it with new campgrounds
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
