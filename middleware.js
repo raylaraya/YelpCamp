bf787f6c020d6a6feb0cd7c993c84e9ef3bcee48
raylaraya <raymondlaraya@gmail.com>
2023-06-27 17:26:28 -0400

    Implement middleware to check if user has signed in
    If a user is not authenticated they may not view certain pages or make
    requests such as through postman

---
+++ middleware.js
@@ -0,0 +1,9 @@
+// isAuthenticated checks if the user is signed in. If the user is not
+// signed in they may not view the page
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.flash('error', 'You must be signed in');
+        return res.redirect('/login');
+    }
+    next();
+}
