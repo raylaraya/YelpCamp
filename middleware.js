d7a34e97f5b443e31da981dadb04ab85ebc5f7c0
raylaraya <raymondlaraya@gmail.com>
2023-06-29 22:44:22 -0400

    Create middleware to check if user has authorization to edit/ delete campground
    Refactored campgrounds.js and reviews.js and moved middleware into the
    middleware file. Created a middleware for the campground permissions/ authorization

--- middleware.js
+++ middleware.js
@@ -1,3 +1,7 @@
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
+const ExpressError = require('./utils/ExpressError');
+const Campground = require('./models/campground');
+
 // isAuthenticated checks if the user is signed in. If the user is not
 // signed in they may not view the page
 module.exports.isLoggedIn = (req, res, next) => {
@@ -12,6 +16,43 @@ module.exports.isLoggedIn = (req, res, next) => {
 module.exports.storeReturnTo = (req, res, next) => {
     if (req.session.returnTo) {
         res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+
+// middleware to validate reviews
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+// middleware to validate campground data
+module.exports.validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+// middleware to check if user is the author of a campground
+module.exports.isAuthor = async (req, res, next) => {
+    // take id from url
+    const { id } = req.params;
+    // find campground by that id
+    const campground = await Campground.findById(id);
+    // compares id of user currently signed in to the id of the author of the campground
+    // if the id's are not the same we flash an error message and redirect
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
     }
     next();
 }
