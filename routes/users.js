c186b8c65f558be4ae40fc29e66ea1f6081c22c7
raylaraya <raymondlaraya@gmail.com>
2023-07-02 18:47:10 -0400

    Restructure campgrounds and users routes

--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
-const User = require('../models/user');
 const users = require('../controllers/users');
 const { storeReturnTo } = require('../middleware');
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register));
-
-// serving form
-router.get('/login', users.renderLogin);
-
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo, passport.authenticate(
+        'local', { failureFlash: true, failureRedirect: '/login' }),
+        users.login);
 
 router.post('/logout', users.logout);
 
