6ae0e5caeb063c63868fa86cadf5efff0b373e29
raylaraya <raymondlaraya@gmail.com>
2023-07-11 01:37:50 -0400

    Fixed seeds file to automatically geocode campgrounds
    When we re-seed our data base the campground locations will be geocoded and
    will be properly displayed on the map.

--- seeds/index.js
+++ seeds/index.js
@@ -5,10 +5,19 @@
     In our case we our populating our mongodb database with initial sets of campgrounds
 */
 
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const mongoose = require('mongoose');
 const cities = require('./cities')
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
+
+// import mapbox
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -25,28 +34,35 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 // deletes everything from our database and replaces it with new campgrounds
 const seedDB = async () => {
-    await Campground.deleteMany({});
+    await Campground.deleteMany({}); // delete everything
+
+    // Seed 50 new camps
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const name = `${sample(descriptors)} ${sample(places)}`;
         const price = Math.floor(Math.random() * 20) + 10;
+        const location = `${cities[random1000].city}, ${cities[random1000].state}`
+        const geoData = await geocoder.forwardGeocode({
+            query: location,
+            limit: 1
+        }).send()
+
         const camp = new Campground({
+            // Your user ID. If you delete the users in mongoDB you will get an error when trying to seed your database
             author: '6499fc31aab7dc54feac6c83',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci iste modi, minus eum quis nemo et fugiat, doloremque eveniet nobis natus commodi odit aliquid quia totam voluptatum quasi sint corporis!',
+            title: name,
+            location: location,
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magni distinctio porro, cum dicta perspiciatis nam? Ad maiores, nisi voluptatum obcaecati magni neque omnis ab et nobis amet molestias exercitationem.",
             price,
-            geometry: {
-                type: "Point",
-                coordinates: [-113.1331, 47.0202],
-            },
+            geometry: geoData.body.features[0].geometry,
             images: [
                 {
-                    url: 'https://res.cloudinary.com/dpahchhsg/image/upload/v1688688747/YelpCamp/otsnebomcbjousi1vguf.jpg',
-                    filename: 'YelpCamp/otsnebomcbjousi1vguf',
+                    url: 'https://res.cloudinary.com/dpahchhsg/image/upload/v1688683847/YelpCamp/wybwqj5vnagmyxfq7i6x.jpg',
+                    filename: 'YelpCamp/wybwqj5vnagmyxfq7i6x'
                 },
                 {
                     url: 'https://res.cloudinary.com/dpahchhsg/image/upload/v1688683848/YelpCamp/ldmmtrs23gcjtwwfzlpi.jpg',
-                    filename: 'YelpCamp/ldmmtrs23gcjtwwfzlpi',
+                    filename: 'YelpCamp/ldmmtrs23gcjtwwfzlpi'
                 }
             ]
         })
